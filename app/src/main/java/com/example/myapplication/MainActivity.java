5521dadfcf8deae77c5b328dcab46ad72c0523c7
sbq <517922287@qq.com>
2020-10-21 17:18:58 +0800

    第三次提交版本

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println(111111);
+        System.out.println(2222);
         setContentView(R.layout.activity_main);
     }
 }
